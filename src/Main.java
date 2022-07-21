0d937fd6a5fe8abd4a11042ea5e4254f9ef4f277
Amax-23 <maksim.agoshkov@gmail.com>
2022-07-21 20:51:30 +0300

    My second commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] resultSale = new int[3];
+        for(int i = 0; i < resultSale.length; i++) {
+            resultSale[i] = 5;
+        }
+        SalesManager salesManager = new SalesManager(resultSale);//создане объекта класса SalesManager
+        System.out.println(Arrays.toString(resultSale)); // демонстрация содержимого массива
+        System.out.println(salesManager.max());// результат использования объектом метода max
+
+
+    }
+}
