74ad40194797d10d28230f346f421200c311b0c8
Amax-23 <maksim.agoshkov@gmail.com>
2022-07-26 11:33:29 +0300

    Поправил код по замечаниям Филиппа Воронова

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.println("Домашнее задание по теме «Внедрение системы контроля версий");
-//        long[] resultSale = new long[3];
-//        for(int i = 0; i < resultSale.length; i++) {
-//            resultSale[i] = 5;
-//        }
-//        SalesManager salesManager = new SalesManager(resultSale);//создане объекта класса SalesManager
-//        System.out.println(Arrays.toString(resultSale)); // демонстрация содержимого массива
-//        System.out.println(salesManager.max());// результат использования объектом метода max
-//        System.out.println();
-
         System.out.println("Домашнее задание по теме «История работы и ветки»");
         long[] resultSales = {5, 10, 15};
         SalesManager salesManager1 = new SalesManager(resultSales);
-        System.out.println(salesManager1.max() +
-                " - максимальное значение по продажам");
-        System.out.println(salesManager1.averageValue() +
-                " - среднее значение по продажам");//результат использования объектом метода averageValue
+        //System.out.println(salesManager1.max() + " - максимальное значение по продажам");
+        System.out.println(salesManager1.average() + " - среднее значение по продажам");
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,8 +1,10 @@
 public class SalesManager {
     protected long[] sales;
+
     public SalesManager(long[] sales) {
         this.sales = sales;
     }
+
     public long max() {
         long max = -1;
         for (long sale : sales) {
@@ -12,13 +14,14 @@ public class SalesManager {
         }
         return max;
     }
-    public long averageValue (){   // метод расчета среднего значения
+
+    public long average() {   // метод расчета среднего значения
         int totalSales = 0;
         long sum = 0;
-        for (int i = 0; i < sales.length; i++){
-          sum = sum + sales[i];
-          totalSales++;
-          }
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+            totalSales++;
+        }
         return sum / totalSales;
     }
 }
